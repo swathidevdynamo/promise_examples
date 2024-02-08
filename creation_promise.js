a533ec7080208bf17c5369499f78230ab1ad13a3
swathiy <swathi.yenika73@gmail.com>
2024-02-08 14:32:03 +0100

    promise examples

---
+++ creation_promise.js
@@ -0,0 +1,11 @@
+new Promise(function exec(resolve,reject){
+    //this is creation of promise object and promise creation is synchronous in nature
+    //Promise is nothing but an object in javascript
+    //Promise has 3 states pending, fulfilled, rejection
+    /**
+     * When Promise is in pending state - value will be "undefined" and 
+     * Pending - undefined
+     * Fulfilled - value given in resolve func argument
+       Rejection - value given in reject func argument 
+    **/
+})
---
+++ promise_example1.js
@@ -0,0 +1,26 @@
+function getRandomInt(max) {
+    console.log("getRandomInt ----------", max)
+    return Math.floor(Math.random * max)
+}
+
+function examplewithforloop() {
+    return new Promise(function executor(resolve, reject) {
+        for (let i = 0; i < 100000000000; i++) {}
+        let num = getRandomInt(10);
+        console.log("num value is ----------", num)
+        if (num % 2 == 0) {
+            // resolve promise
+            resolve(num);
+            console.log("resolving promise")
+        } else {
+            // reject promise
+            reject(num)
+            console.log("rejecting promise")
+        }
+    })
+}
+
+let x = examplewithforloop();
+// examplewithforloop function has code which is synchronous in nature similar to javascript
+
+console.log("promise synchronous code--------->>>>>>.", x)
